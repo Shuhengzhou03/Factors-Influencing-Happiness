00b4b7a27fc07da92b77e7482709c2d4ac6a55f6
Shuhengzhou03 <shuheng.zhou@mail.utoronto.ca>
2024-12-03 00:04:37 -0500

    Update 02-download_data.R

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from GSS
 # Author: Shuheng (Jack) Zhou
-# Date: November 26 2024
+# Date: November 27 2024
 # Contact: shuhengzhou5@gmail.com
 # License: MIT
 # Pre-requisites: None
